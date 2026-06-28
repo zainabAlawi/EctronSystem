3e9d5fc43e3db8b37206f9c8f4969229bcaba21b
zainabAlawi <zainabalalawinec@gmail.com>
2026-06-28 12:01:58 +0400

    Add Vercel configuration

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.get('/api/production-chart', (req, res) => {
     res.json(data);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
